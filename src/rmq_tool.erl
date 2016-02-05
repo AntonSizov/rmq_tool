17a48d101c9b7fc9fc39af33a9d72fbb8bfd099d
Anton Sizov <a.sysoff@gmail.com>
2016-02-05 16:30:00 +0300

    Remove inject by id, list dumps and shell help

--- src/rmq_tool.erl
+++ src/rmq_tool.erl
@@ -19,9 +19,7 @@
     inject/4,
 
     purge/1,
-    delete_queue/1,
-    list_dumps/0,
-    help/0
+    delete_queue/1
 ]).
 
 
@@ -42,6 +40,7 @@
 -spec main([list()]) -> ignore.
 main(Args) ->
     {ok, {Props, CmdAndArgs}} = getopt:parse(?OptSpecList, Args),
+    ?log_debug("CmdAndArgs: ~p", [CmdAndArgs]),
     main(Props, CmdAndArgs).
 
 main(Props, ["purge", QueueName]) ->
@@ -56,18 +55,10 @@ main(Props, ["dump", QueueName, N]) ->
     start_all(Props),
     dump(list_to_binary(QueueName), list_to_integer(N)),
     halt(0);
-main(_Props, ["list_dumps"]) ->
-    list_dumps(),
-    halt(0);
 main(Props, ["restore", QueueName0, DumpIdOrFileName]) ->
     start_all(Props),
     QueueName = list_to_binary(QueueName0),
-    try list_to_integer(DumpIdOrFileName) of
-        DumpId ->
-            inject(QueueName, DumpId)
-    catch _:_ ->
-        inject(QueueName, list_to_binary(DumpIdOrFileName))
-    end,
+    inject(QueueName, list_to_binary(DumpIdOrFileName)),
     halt(0);
 main(Props, ["restore", QueueName, DumpFileName, Offset]) ->
     start_all(Props),
@@ -83,7 +74,7 @@ main(Props, ["delete_queue", QueueName]) ->
     halt(0);
 main(_Props, _) ->
     MainOptionsDescr = [
-        {"command", "purge, dump, list_dumps, restore, delete_queue"},
+        {"command", "purge, dump, restore, delete_queue"},
         {"command_args", ""}
     ],
     getopt:usage(?OptSpecList, "rmq_tool", "<command> [<command_args>]", MainOptionsDescr),
@@ -100,12 +91,6 @@ main(_Props, _) ->
 
     "\tDump N messages in queue: ~n"
     "\trmq_tool dump <queue_name> <N>~n~n"
-
-    "\tList available dumps: ~n"
-    "\trmq_tool list_dumps~n~n"
-
-    "\tRestore messages from queue dump by dump id: ~n"
-    "\trmq_tool restore <queue_name> <dump_id>~n~n"
 
     "\tRestore messages from queue dump by dump file name: ~n"
     "\trmq_tool restore <queue_name> <dump_file_name>~n~n"
@@ -167,20 +152,12 @@ dump(QueueName, Max, NoAck) ->
 
 %% @doc injecting queue with all data taken from a file
 -spec inject(QueueName :: binary(), FileName :: string()) -> ok.
-inject(QueueName, DumpNumber) when is_integer(DumpNumber) ->
-    {ok, Dumps} = get_dump_list(),
-    FileName = lists:nth(DumpNumber, Dumps),
-    inject(QueueName, FileName);
 inject(QueueName, FileName) ->
     rmq_inject:inject(QueueName, FileName).
 
 
 %% @doc injecting queue with all data taken from a file. Skipping a couple of starting messages
 -spec inject(QueueName :: binary(), FileName :: string(), Offset :: integer()) -> ok.
-inject(QueueName, DumpNumber, Offset) when is_integer(DumpNumber) ->
-    {ok, Dumps} = get_dump_list(),
-    FileName = lists:nth(DumpNumber, Dumps),
-    inject(QueueName, FileName, Offset);
 inject(QueueName, FileName, Offset) ->
     rmq_inject:inject(QueueName, FileName, Offset).
 
@@ -189,49 +166,6 @@ inject(QueueName, FileName, Offset) ->
 -spec inject(QueueName :: binary(), FileName :: string(), Offset :: integer(), Count :: integer()) -> ok.
 inject(QueueName, FileName, Offset, Count) ->
     rmq_inject:inject(QueueName, FileName, Offset, Count).
-
-%% @doc List all available dump files
--spec list_dumps() -> ok.
-list_dumps() ->
-    {ok, Dumps} = get_dump_list(),
-    Print = fun(N) ->
-        Name = lists:nth(N, Dumps),
-        io:format("~p: ~p~n", [N, Name])
-    end,
-    lists:foreach(Print, lists:seq(1, length(Dumps))).
-
-get_dump_list() ->
-    RawList = os:cmd("ls ./dumps"),
-    ListOfDumps = string:tokens(RawList, "\n"),
-    {ok, ListOfDumps}.
-
-%% @doc Print help info
--spec help() -> ok.
-help() ->
-    io:format(
-    "Purge queue: ~n"
-    "rmq_tool:purge(<<\"pmm.mmwl.response.sms\">>).~n~n"
-
-    "Dupm all messages in queue: ~n"
-    "rmq_tool:dump(<<\"pmm.mmwl.response.sms\">>).~n~n"
-
-    "Dump 1000 messages in queue: ~n"
-    "rmq_tool:dump(<<\"pmm.mmwl.response.sms\">>, 1000).~n~n"
-
-    "List available dumps: ~n"
-    "rmq_tool:list_dumps().~n~n"
-
-    "Inject all messages into queue: ~n"
-    "rmq_tool:inject(<<\"pmm.mmwl.response.sms\">>, 1) %% 1 is the number of the dump from list_dump listing~n"
-    "rmq_tool:inject(<<\"pmm.mmwl.response.sms\">>, \"pmm.mmwl.response.sms_20121022_17184.qdump\")~n~n"
-
-    "Advanced inject messages into queue: ~n"
-    "rmq_tool:inject(QueueName, FileName, Offset)~n"
-    "rmq_tool:inject(QueueName, FileName, Offset, Count)~n~n"
-
-    "Delete queue: ~n"
-    "rmq_tool:delete_queue(<<\"queue_name\">>).~n"
-    ).
 
 
 start_all(Props) ->
